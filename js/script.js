e7df4d0e1a199dfc1111950a2b30f83acdf6751e
juniorjc1533 <ti1@fanex.com.br>
2025-07-22 21:54:44 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+// Validação do Login
+$(document).ready(function() {
+    $("#loginForm").submit(function(e) {
+        e.preventDefault();
+        const email = $("#email").val();
+        const senha = $("#senha").val();
 
+        if (email === "admin@example.com" && senha === "123456") {
+            alert("Login realizado com sucesso!");
+            window.location.href = "index.html";
+        } else {
+            alert("E-mail ou senha incorretos!");
+        }
+    });
+});
+
+// Efeitos no carrossel (opcional)
+$('.carousel').carousel({
+    interval: 3000
+});
